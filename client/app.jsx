b03953413302d897a10d6c362c8bc372accd06be
febriadj <iamfebriadji@gmail.com>
2022-10-16 16:30:17 +0700

    chore: Add client basic files

---
+++ client/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+function App() {
+  return (
+    <h1>Hello, World!</h1>
+  );
+}
+
+export default App;
---
+++ client/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+// React 18x root APIs
+import * as ReactDOM from 'react-dom/client';
+import App from './app';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<App />);
