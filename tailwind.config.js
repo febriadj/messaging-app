ed8742c84aaf9d0b5513384e01bab62c32b3f6c8
febriadj <iamfebriadji@gmail.com>
2022-10-20 15:34:22 +0700

    feat: Add colors.spill

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const { colors: defaultColors } = require('tailwindcss/defaultTheme');
 const scrollbar = require('tailwind-scrollbar');
 
 module.exports = {
@@ -7,6 +8,22 @@ module.exports = {
     extend: {
       fontFamily: {
         display: 'Lobster Two',
+      },
+      colors: {
+        ...defaultColors,
+        spill: {
+          DEFAULT: '#151C21',
+          50: '#F6F8F9',
+          100: '#EEF0F1',
+          200: '#E1E3E5',
+          300: '#D0D4D7',
+          400: '#ACB3B9',
+          500: '#7D858C',
+          600: '#475057',
+          700: '#31393F',
+          800: '#1F272E',
+          900: '#151C21',
+        },
       },
     },
   },
