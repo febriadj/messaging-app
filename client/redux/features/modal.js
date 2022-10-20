5ee1a3cdf1449d7d12fc7e39d62e304e1a8815c0
febriadj <iamfebriadji@gmail.com>
2022-10-20 16:58:08 +0700

    chore(redux): Add modal slice

---
+++ client/redux/features/modal.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const ModalSlice = createSlice({
+  name: 'modal',
+  initialState: {
+    minibox: false,
+  },
+  reducers: {
+    setModal(state, action) {
+      const { target = null, data = null } = action.payload;
+
+      if (target) {
+        Object.keys(state).forEach((key) => {
+          if (target === key) {
+            state[target] = data || !state[target];
+          } else {
+            state[key] = false;
+          }
+        });
+      }
+    },
+  },
+});
+
+export const { setModal } = ModalSlice.actions;
+export default ModalSlice.reducer;
