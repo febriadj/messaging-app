5a637b66562355c66b5db5255550041397680dcf
febriadj <iamfebriadji@gmail.com>
2022-10-20 16:49:40 +0700

    feat(components/chat): Add foreground.minibox component

---
+++ client/components/chat/foreground/minibox.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as bi from 'react-icons/bi';
+import { setModal } from '../../../redux/features/modal';
+
+function Minibox() {
+  const dispatch = useDispatch();
+  const { minibox } = useSelector((state) => state.modal);
+
+  return (
+    <div
+      className={`
+        ${minibox ? 'opacity-100 z-10' : 'opacity-0 -z-50 scale-50'}
+        transition duration-75 absolute right-0 translate-y-12 -translate-x-6 shadow-xl rounded-md
+        bg-white dark:bg-spill-900 dark:text-white/90
+      `}
+    >
+      <div className="py-2 grid">
+        {
+          [
+            { target: 'profile', html: 'Profile', icon: <bi.BiUserCircle /> },
+            { target: 'starred', html: 'Starred Message', icon: <bi.BiStar /> },
+            { target: 'setting', html: 'Setting', icon: <bi.BiCog /> },
+            { target: 'signout', html: 'Sign out', icon: <bi.BiLogOutCircle /> },
+          ].map((elem) => (
+            <button
+              type="button"
+              key={elem.target}
+              className="py-2 px-4 flex gap-2 items-center hover:bg-spill-200/40"
+              onClick={() => {
+                dispatch(setModal({
+                  target: elem.target,
+                }));
+              }}
+            >
+              <i className="opacity-20">{elem.icon}</i>
+              <p>{elem.html}</p>
+            </button>
+          ))
+        }
+      </div>
+    </div>
+  );
+}
+
+export default Minibox;
--- client/containers/chat/foreground.jsx
+++ client/containers/chat/foreground.jsx
@@ -4,6 +4,7 @@ import * as comp from '../../components/chat/foreground';
 function ForeGround() {
   return (
     <div className="relative z-10 grid grid-rows-[auto_1fr] overflow-hidden">
+      <comp.minibox />
       <comp.header />
     </div>
   );
