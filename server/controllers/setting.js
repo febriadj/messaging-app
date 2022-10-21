45cabb081a6c93b31a97782119229b09f7f54501
febriadj <iamfebriadji@gmail.com>
2022-10-21 23:12:37 +0700

    feat(api): Add router to update account settings

--- server/controllers/setting.js
+++ server/controllers/setting.js
@@ -18,3 +18,26 @@ exports.find = async (req, res) => {
     });
   }
 };
+
+exports.update = async (req, res) => {
+  try {
+    const setting = await SettingModel.updateOne(
+      { userId: req.user._id },
+      { ...req.body },
+    );
+
+    response({
+      res,
+      message: 'Successfully updated account settings',
+      payload: setting,
+    });
+  }
+  catch (error0) {
+    response({
+      res,
+      statusCode: error0.statusCode || 500,
+      success: false,
+      message: error0.message,
+    });
+  }
+};
--- server/routes/setting.js
+++ server/routes/setting.js
@@ -3,5 +3,6 @@ const authenticate = require('../middleware/auth');
 const ctrl = require('../controllers/setting');
 
 router.get('/settings', authenticate, ctrl.find);
+router.put('/settings', authenticate, ctrl.update);
 
 module.exports = router;
