171e31ad90544b5191597c6d4c1596acb7eceab7
febriadj <iamfebriadji@gmail.com>
2022-10-29 23:23:23 +0700

    refactor: Update code in monitor component

--- client/components/chat/room/monitor.jsx
+++ client/components/chat/room/monitor.jsx
@@ -26,15 +26,14 @@ function Monitor({ chats, setChats }) {
   }, []);
 
   return (
-    <div id="monitor" className="flex justify-center overflow-y-auto bg-spill-100 dark:bg-spill-950 scrollbar-thin scrollbar-thumb-spill-300 hover:scrollbar-thumb-spill-400 dark:scrollbar-thumb-spill-800 dark:hover:scrollbar-thumb-spill-700">
-      <div className="relative w-full max-w-[560px] py-2 px-6 flex flex-col gap-1">
+    <div id="monitor" className="overflow-y-auto bg-spill-100 dark:bg-spill-950 scrollbar-thin scrollbar-thumb-spill-300 hover:scrollbar-thumb-spill-400 dark:scrollbar-thumb-spill-800 dark:hover:scrollbar-thumb-spill-700">
+      <div className="relative py-2 flex flex-col">
         {
-          // chats
           chats && chats.map((elem, i, arr) => (
-            <div key={elem._id}>
+            <React.Fragment key={elem._id}>
               {
-                 moment(elem.createdAt).date()
-                 !== (i > 0 && moment(arr[i - 1].createdAt).date())
+                // chat header: show datetime every new days
+                moment(elem.createdAt).date() !== (i > 0 && moment(arr[i - 1].createdAt).date())
                   && (
                     <div className="my-4 flex justify-center">
                       <span className="block py-0.5 px-2 rounded-full bg-white dark:bg-spill-800">
@@ -43,55 +42,52 @@ function Monitor({ chats, setChats }) {
                     </div>
                   )
               }
-              <div
-                className={`
-                  ${i < arr.length - 1 && (elem.userId !== arr[i + 1].userId) && 'mb-4'}
-                  ${i === arr.length - 1 && 'mb-6'}
-                  ${elem.userId === master._id ? 'justify-end' : 'justify-start'}
-                  flex
-                `}
-              >
-                <div
-                  className={`
-                    ${elem.userId === master._id ? 'ml-12 rounded-l-xl bg-sky-200 dark:bg-sky-400/40' : 'mr-12 rounded-r-xl bg-white dark:bg-spill-800'}
-                    ${i > 0 && (elem.userId === arr[i - 1].userId) && 'rounded-xl'}
-                    relative p-2 rounded-b-xl grid gap-2
-                  `}
-                >
-                  { elem.replyTo && (
-                    <div className="rounded-md flex overflow-hidden dark:bg-spill-900/40">
-                      <span className="block w-1 h-full bg-sky-200"></span>
-                      <span className="p-2 text-sm">
-                        <p className="font-bold">{elem.reply.username}</p>
-                        <p className="opacity-60">{elem.reply.text}</p>
+              <div className={`${elem.userId !== arr[i + 1]?.userId && 'mb-4'} w-full py-0.5 px-6 flex justify-center`}>
+                <div className={`${elem.userId === master._id && 'justify-end'} w-[560px] flex`}>
+                  {/* chat card */}
+                  <div
+                    className={`
+                      ${elem.userId === master._id ? ' ml-12 rounded-l-xl bg-sky-200 dark:bg-sky-400/40' : 'mr-12 rounded-r-xl bg-white dark:bg-spill-800'}
+                      ${elem.userId === arr[i - 1]?.userId && moment(elem.createdAt).date() === moment(arr[i - 1]?.createdAt).date() && 'rounded-xl'}
+                      relative p-2 rounded-b-xl overflow-hidden
+                    `}
+                  >
+                    {
+                      elem.replyTo && (
+                        <div className="relative mb-2 rounded-xl grid grid-cols-[auto_1fr] overflow-hidden bg-black/5 dark:bg-black/20">
+                          <span className="block w-1 h-full bg-sky-600 dark:bg-sky-200"></span>
+                          <span className="py-2 px-3">
+                            <span className="text-sm">{elem.reply.fullname}</span>
+                            <p className="text-sm opacity-60">{elem.reply.text}</p>
+                          </span>
+                        </div>
+                      )
+                    }
+                    {/* chat body message */}
+                    <div className="px-1">
+                      <p className="break-all">
+                        <Linkify as="span">{elem.text}</Linkify>
+                        <span className={`${elem.userId === master._id && 'mr-5'} invisible text-xs ml-1`}>{moment(elem.createdAt).format('LT')}</span>
+                      </p>
+                      <span className="p-2 absolute bottom-0 right-0 flex gap-0.5 items-center">
+                        <p className="text-xs opacity-80">{moment(elem.createdAt).format('LT')}</p>
+                        {
+                          elem.userId === master._id && (
+                            <i>
+                              {
+                                elem.readed
+                                  ? <ri.RiCheckDoubleFill size={18} className="text-sky-600 dark:text-sky-400" />
+                                  : <ri.RiCheckFill size={18} className="opacity-80" />
+                              }
+                            </i>
+                          )
+                        }
                       </span>
                     </div>
-                  ) }
-                  <div className="px-2">
-                    <p className="[&>a]:text-sky-200 break-all">
-                      <Linkify as="span">{elem.text}</Linkify>
-                      <span className={`${elem.userId === master._id && 'mr-5'} inline-block invisible text-xs ml-0.5`}>
-                        {moment(elem.createdAt).format('LT')}
-                      </span>
-                    </p>
-                    <span className="p-2 absolute bottom-0 right-0 flex gap-0.5 items-center">
-                      <p className="text-xs opacity-80">{moment(elem.createdAt).format('LT')}</p>
-                      {
-                        elem.userId === master._id && (
-                          <i>
-                            {
-                              elem.readed
-                                ? <ri.RiCheckDoubleFill size={18} className="text-sky-600 dark:text-sky-400" />
-                                : <ri.RiCheckFill size={18} className="opacity-80" />
-                            }
-                          </i>
-                        )
-                      }
-                    </span>
                   </div>
                 </div>
               </div>
-            </div>
+            </React.Fragment>
           ))
         }
       </div>
