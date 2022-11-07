8bb54a2eae429e251202aa7b4a5210fcaff60ae4
febriadj <iamfebriadji@gmail.com>
2022-11-07 10:00:55 +0700

    feat: Add a change password modal

--- client/pages/chat.jsx
+++ client/pages/chat.jsx
@@ -16,6 +16,7 @@ function Chat() {
       }}
     >
       <comp.signout />
+      <comp.changePass />
       <comp.deleteAcc />
 
       <cont.foreground />
--- client/redux/features/modal.js
+++ client/redux/features/modal.js
@@ -6,6 +6,7 @@ const ModalSlice = createSlice({
     minibox: false,
     signout: false,
     newcontact: false,
+    changePass: false,
     deleteAcc: false,
   },
   reducers: {
