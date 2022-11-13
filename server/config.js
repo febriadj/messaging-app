d55e3d86d6b122967489539be9cf3049cc6cdae1
febriadj <iamfebriadji@gmail.com>
2022-11-13 09:36:11 +0700

    fix: Update config.js for server

--- server/config.js
+++ server/config.js
@@ -1,6 +1,8 @@
+const isDev = process.env.NODE_ENV === 'development';
+
 module.exports = {
-  isDev: process.env.NODE_ENV === 'development',
-  host: 'https://www.spill.com',
+  isDev,
+  host: isDev ? 'http://localhost:8080' : 'https://www.spillgram.com',
   db: {
     uri: process.env.MONGO_URI,
     name: 'spill',
