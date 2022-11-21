5ce80d1869a7619cbbe66c7272bfd3cf437918b9
febriadj <iamfebriadji@gmail.com>
2022-11-21 09:24:21 +0700

    feat(socket): Set online status when user 'connect' or 'disconnect' events are executed

--- client/components/chat/room/private/header.jsx
+++ client/components/chat/room/private/header.jsx
@@ -4,6 +4,7 @@ import moment from 'moment';
 import * as bi from 'react-icons/bi';
 import { setChatRoom } from '../../../../redux/features/room';
 import { setPage } from '../../../../redux/features/page';
+import socket from '../../../../helpers/socket';
 
 function Header() {
   const dispatch = useDispatch();
@@ -26,6 +27,48 @@ function Header() {
   useEffect(() => {
     handleSubhead();
   }, [chatRoom.isOpen, chatRoom.refreshId]);
+
+  const setOnlineStatus = (args) => {
+    dispatch(setChatRoom({
+      isOpen: true,
+      refreshId: chatRoom.refreshId,
+      data: {
+        ...chatRoom.data,
+        profile: {
+          ...chatRoom.data.profile,
+          ...args,
+        },
+      },
+    }));
+  };
+
+  useEffect(() => {
+    // user online
+    socket.on('user/connect', (userId) => {
+      if (userId === chatRoom.data.profile.userId) {
+        setSubhead('online');
+        setOnlineStatus({ online: true });
+      }
+    });
+
+    // user offline
+    socket.on('user/disconnect', (userId) => {
+      if (userId === chatRoom.data.profile.userId) {
+        const updatedAt = new Date().toISOString();
+
+        setSubhead(`last seen ${moment(updatedAt).fromNow()}`);
+        setOnlineStatus({
+          online: false,
+          updatedAt,
+        });
+      }
+    });
+
+    return () => {
+      socket.off('user/connect');
+      socket.off('user/disconnect');
+    };
+  }, []);
 
   return (
     <div className="h-16 px-2 md:pl-4 flex gap-2 items-center bg-white dark:bg-spill-900">
--- server/socket/events/user.js
+++ server/socket/events/user.js
@@ -1,6 +1,26 @@
+const ProfileModel = require('../../db/models/profile');
+
 module.exports = (socket) => {
   // user connect
-  socket.on('user/connect', (userId) => {
+  socket.on('user/connect', async (userId) => {
     socket.join(userId);
+
+    /* eslint-disable */
+    // store userId in socket object
+    socket.userId = userId;
+    /* eslint-enable */
+
+    await ProfileModel.updateOne({ userId }, { $set: { online: true } });
+
+    socket.broadcast.emit('user/connect', userId);
+  });
+
+  // user disconnect
+  socket.on('disconnect', async () => {
+    const { userId } = socket;
+
+    await ProfileModel.updateOne({ userId }, { $set: { online: false } });
+
+    socket.broadcast.emit('user/disconnect', userId);
   });
 };
